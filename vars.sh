2286105356dc418aa0e0281b132827756d65e856
Keira Monuki <ksmonuki@gmail.com>
2019-05-30 14:03:02 -0700

    Homework Week 8

---
+++ vars.sh
@@ -0,0 +1,24 @@
+#vars file
+#path to directory with R file (edited by user)
+DB="/Users/keiramonuki/Documents/C177/Project/"
+#variable for estimate function
+EST="Species,MaxLengthTL,Troph,seTroph,TrophObserved"
+# SPE="Species"
+# ML="MaxLengthTL"
+# TPH="Troph"
+# seTPH="seTroph"
+# TPHO="TrophObserved"
+#
+#variable for predator function
+PRE="Species,PredatorGroup,PredatorName"
+# PRGP="PredatorGroup"
+# PRNM="PredatorName"
+#
+#variable for stocks
+STO="Species,StockDefsGeneral,IUCN_Code,EnvTemp,Resilience,GenBankID,IUCN_ID"
+# STCK="StockDefsGeneral"
+# IUCD="IUCN_Code"
+# ENV="EnvTemp"
+# RES="Resilience"
+# GEN="GenBankID"
+# IUID="IUCN_ID"
